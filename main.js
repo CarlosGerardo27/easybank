fb3de5ea265f5c9317d58f1e68841874d6d38800
Carlos Gerardo RS <carlosgerardo.rocha.s@gmail.com>
2020-07-12 21:25:45 -0500

    Reto Finalizados

--- main.js
+++ main.js
@@ -1,3 +1,44 @@
 'use strict'
+var facebook = document.getElementById("facebook");
+var youtube = document.getElementById("youtube");
+var twitter = document.getElementById("twitter");
+var pinterest = document.getElementById("pinterest");
+var instagram = document.getElementById("instagram");
 
-var social = document.getElementsByClassName("svg")
+
+
+facebook.addEventListener("mouseover",function(){
+    facebook.style.fill="#2BB7DA"
+})
+facebook.addEventListener("mouseleave",function(){
+    facebook.style.fill="#fff"
+})
+
+youtube.addEventListener("mouseover",function(){
+    youtube.style.fill="red"
+})
+youtube.addEventListener("mouseleave",function(){
+    youtube.style.fill="#fff"
+})
+
+twitter.addEventListener("mouseover",function(){
+    twitter.style.fill="#2BB7DA"
+})
+twitter.addEventListener("mouseleave",function(){
+    twitter.style.fill="#fff"
+})
+
+pinterest.addEventListener("mouseover",function(){
+    pinterest.style.fill="red"
+})
+pinterest.addEventListener("mouseleave",function(){
+    pinterest.style.fill="#fff"
+})
+
+instagram.addEventListener("mouseover",function(){
+    instagram.style.fill="pink"
+})
+instagram.addEventListener("mouseleave",function(){
+    instagram.style.fill="#fff"
+})
+
